8e08693e4fd4b9cc6d74960d89c7ab39f4682a64
joseph-jomon <josephjomon401@gmail.com>
2024-10-29 21:07:32 +0100

    minimized it

--- myapp/tasks/image_batch_processor.py
+++ myapp/tasks/image_batch_processor.py
@@ -10,7 +10,7 @@ import io
 
 # Class that inherits from BatchProcessor to handle image batch processing
 class ImageBatchProcessor(BatchProcessor):
-    def __init__(self, batch_size=10):
+    def __init__(self, batch_size=5):
         """
         Initializes the ImageBatchProcessor with a given batch size.
         Args:
@@ -78,7 +78,7 @@ class ImageBatchProcessor(BatchProcessor):
 
 # Define a Celery task to  process a batch of image data
 @app.task
-def process_image_batch_task(images_with_ids, company_name, batch_size=10):
+def process_image_batch_task(images_with_ids, company_name, batch_size=5):
     """
     Celery task to process a batch of image data and generate embeddings.
     Args:
